051077e83674259beabb0b71ede196cc6c6d5135
Andrew Stone <andstone@gmail.com>
2012-11-24 19:26:13 -0800

    First push. Product object and scaffolding implemented.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'de6d6bbd842a0335d9d00c148cab3fc3298169f15d473611238a4e347875c1ef919912eeb8ba36f40abcb62fb2964ece5fc3010946b98758607e8919d75ffa5d'
