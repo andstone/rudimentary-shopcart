8567084777a06cd6a6a808804bd168d26ee82313
Andrew Stone <andstone@gmail.com>
2012-11-25 19:08:37 -0800

    Created store's catalog display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def load_time
+  	return Time.now.strftime('%m/%d/%y - %I:%M %p')
+  end
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -16,6 +16,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#main tr', minimum: 1
   end
 
   test "should get new" do
@@ -53,4 +54,6 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+
 end
